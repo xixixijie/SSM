85f5c84b7b0fa048a67cede2bf6e35d8e79fe060
xixi <458927520@qq.com>
2018-07-22 15:45:47 +0800

    cxj commit 2018/07/22 3:43 product

--- src/com/ssm/model/dao/ProductDAO.java
+++ src/com/ssm/model/dao/ProductDAO.java
@@ -1,8 +1,10 @@
 package com.ssm.model.dao;
 
+import com.ssm.model.bean.Classify;
 import com.ssm.model.bean.Product;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xixi on 2018/7/19.
@@ -12,4 +14,15 @@ public interface ProductDAO {
     public List<Product> getAllProduct();
 
     public Product getProduct(int pid);
+
+
+
+    public List<Classify> getAllClassify();
+    public void addProduct(Map<String, Object> map);
+    public int getCurrentProductId();
+    public void addAspect(Map<String, Object> map);
+    public void addParameter(Map<String, Object> map);
+    public List<Product> findAllProduct();
+    public Product getProductById(int product_id);
+    public int getAllPageCount();
 }
