99e0716dfee1d317e3626d049de3338d4d595990
fc524079318 <524079318@qq.com>
2018-07-22 20:40:11 +0800

    fc

---
+++ src/com/ssm/model/bean/CustomerService.java
@@ -0,0 +1,22 @@
+package com.ssm.model.bean;
+
+public class CustomerService {
+    private int CsId;
+    private  String CsName;
+
+    public int getCsId() {
+        return CsId;
+    }
+
+    public void setCsId(int csId) {
+        CsId = csId;
+    }
+
+    public String getCsName() {
+        return CsName;
+    }
+
+    public void setCsName(String csName) {
+        CsName = csName;
+    }
+}
