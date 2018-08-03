8bfab035df1b51bf27f1bec9ab9f41753659925d
xixi <458927520@qq.com>
2018-08-03 20:14:26 +0800

    cxj 8.3 8.10

--- src/com/ssm/model/service/ClassifyService.java
+++ src/com/ssm/model/service/ClassifyService.java
@@ -8,6 +8,7 @@ import com.ssm.model.dao.ClassifyDAO;
 import com.ssm.model.dao.KeywordDAO;
 import com.ssm.util.FileUtil;
 
+
 import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
@@ -36,6 +37,7 @@ public class ClassifyService {
 
     /**
      * 删除分类
+     *
      * @param ids
      */
 
@@ -46,6 +48,7 @@ public class ClassifyService {
 
     /**
      * 添加分类
+     *
      * @param classify
      */
 
@@ -57,6 +60,7 @@ public class ClassifyService {
 
     /**
      * 获得分类
+     *
      * @param classifyID
      * @return
      */
@@ -69,6 +73,7 @@ public class ClassifyService {
 
     /**
      * 修改分类
+     *
      * @param classify
      */
 
@@ -84,6 +89,7 @@ public class ClassifyService {
 
     /**
      * 展示分类
+     *
      * @return
      */
 
@@ -103,6 +109,7 @@ public class ClassifyService {
 
     /**
      * 字典序排序
+     *
      * @param list
      * @return
      */
@@ -135,6 +142,7 @@ public class ClassifyService {
 
     /**
      * 根据汉字获得拼音
+     *
      * @param src
      * @return
      */
@@ -168,6 +176,7 @@ public class ClassifyService {
 
     /**
      * 查找分类
+     *
      * @param content
      * @return
      */
@@ -180,6 +189,7 @@ public class ClassifyService {
 
     /**
      * 排序分类
+     *
      * @param type
      * @param ids
      * @return
@@ -216,6 +226,7 @@ public class ClassifyService {
 
     /**
      * 分词
+     *
      * @param adjList
      * @param content
      * @param pipeline
@@ -231,7 +242,7 @@ public class ClassifyService {
 //        pipeline.annotate(document);
 //
 //        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-//        String result="";
+//        String result = "";
 //        for (CoreMap sentence : sentences) {
 //            // traversing the words in the current sentence
 //            // a CoreLabel is a CoreMap with additional token-specific methods
@@ -243,9 +254,9 @@ public class ClassifyService {
 //                // this is the NER label of the token
 //                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
 //                //System.out.println(word + "\t" + pos + "\t" + ne);
-//                result+=word+" ";
+//                result += word + " ";
 //
-//                if(pos.equals("VA"))
+//                if (pos.equals("VA"))
 //                    adjList.add(word);
 //            }
 //
@@ -266,17 +277,27 @@ public class ClassifyService {
 //
 //        //更新语料库
 //        //读取所有评论句子
-//        String[] strings=FileUtil.read();
+//        String[] strings = FileUtil.read();
 //
 //        //所有形容词
-//        List<String> adjList=new ArrayList<>();
-//
+//        List<String> adjList = new ArrayList<>();
+//        System.out.println("句子大小" + strings.length);
 //        //更新xh.txt
-//        for(String string:strings){
-//            FileUtil.write(divide(adjList,string,pipeline),"xh.txt");
+//        for (String string : strings) {
+//            if (string == null)
+//                continue;
+//            FileUtil.write(divide(adjList, string, pipeline), "xh.txt");
 //        }
-//        Set<String> adjSet=new HashSet<>(adjList);
-//
+////        for(String str:adjList){
+////            System.out.println(str);
+////        }
+////        System.out.println(adjList.size());
+//        System.out.println("分词完成");
+//        Set<String> adjSet = new HashSet<>(adjList);
+////        for(String str:adjSet){
+////            System.out.println(str);
+////        }
+////        System.out.println(adjSet.size());
 //        //更新模型
 //        Learn learn = new Learn();
 //        long start = System.currentTimeMillis();
@@ -290,32 +311,39 @@ public class ClassifyService {
 //
 //
 //        //更新关键词标签 以及 关键词库
-//        Word2VEC vec = new Word2VEC() ;
+//        Word2VEC vec = new Word2VEC();
 //        try {
-//            vec.loadJavaModel("/Users/xixi/Desktop/SSM/library/javaVector") ;
+//            vec.loadJavaModel("/Users/xixi/Desktop/SSM/library/javaVector");
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
 //
-//        HashMap<String,float[]> wordMap=vec.getWordMap();
+//        HashMap<String, float[]> wordMap = vec.getWordMap();
 //
+//
+//        for (String str : adjSet) {
+//            System.out.println(str + " " + Arrays.toString(wordMap.get(str)));
+//        }
 //        //DBSCAN聚类
-//        //点数据
-//        List<Point> points=new ArrayList<>();
-//        //填充向量数组
-//        for(String str:adjSet){
-//            Point point=new Point();
+//        //点数组
+//        List<Point> points = new ArrayList<>();
+//        //填充点数组
+//        for (String str : adjSet) {
+//            Point point = new Point();
 //            point.setName(str);
-//            point.setVec(wordMap.get("str"));
+//            point.setVec(wordMap.get(str));
 //            points.add(point);
 //        }
 //
-//        //簇半径
-//        double r=0.4;
+//        System.out.println("点大小" + points.size());
+//
+//        //簇半径 到时候改为0.4
+//        double r = 1;
 //        //簇大小
-//        int clusterSize=2;
+//        int clusterSize = 2;
 //        //簇数量
-//        int clusterNum=DBSCAN(points,r,2);
+//        int clusterNum = DBSCAN(points, r, clusterSize);
+//        System.out.println("簇大小" + clusterNum);
 //
 //        //计算中心词
 //        getCenter(clusterNum,points);
@@ -328,7 +356,8 @@ public class ClassifyService {
 //            }
 //        }
 //
-//        keywordDAO.deleteAll();
+//
+//        //keywordDAO.deleteAll();
 //        //中心点存为 keyLabel
 //
 //        for(Point cp:centerPoint){
@@ -336,8 +365,10 @@ public class ClassifyService {
 //            label.setLabelName(cp.getName());
 //            int num=0;
 //            //获得该中心点的簇的所有点
+//            System.out.println("中心点是"+cp.getName()+"簇类是"+cp.getCid());
 //            List<Point> clu=getCluster(cp,points);
 //            for(Point point:clu){
+//                System.out.println("边缘点是"+point.getName()+"簇类是"+point.getCid());
 //                for(String str:adjList){
 //                    if(str.equals(point.getName())){
 //                        num++;
@@ -345,8 +376,8 @@ public class ClassifyService {
 //                }
 //            }
 //            label.setLabelNum(num);
-//            label.setLabelID(keywordDAO.getMaxLabelID());
-//            keywordDAO.addKeyLabel(label);
+//            //label.setLabelID(keywordDAO.getMaxLabelID());
+//            //keywordDAO.addKeyLabel(label);
 //            //簇内的词存为关键词
 //            for(Point point:clu){
 //                Keyword keyword=new Keyword();
@@ -360,7 +391,7 @@ public class ClassifyService {
 //                }
 //                keyword.setKeyNum(num);
 //
-//                keywordDAO.addKeyword(keyword);
+//                //keywordDAO.addKeyword(keyword);
 //
 //            }
 //
@@ -370,23 +401,20 @@ public class ClassifyService {
 //        }
 //
 //
-//
-//
-//
-//
 //    }
 
     /**
      * 获得相同簇的点
+     *
      * @param p
      * @param points
      * @return
      */
 
-    private List<Point> getCluster(Point p,List<Point> points){
-        List<Point> temp=new ArrayList<>();
-        for(Point point:points){
-            if(point.getCid()==p.getCid())
+    private List<Point> getCluster(Point p, List<Point> points) {
+        List<Point> temp = new ArrayList<>();
+        for (Point point : points) {
+            if (point.getCid() == p.getCid())
                 temp.add(point);
         }
         return temp;
@@ -394,14 +422,15 @@ public class ClassifyService {
 
     /**
      * 获得中心点
+     *
      * @param clusterNum
      * @param points
      */
-    private void getCenter(int clusterNum,List<Point> points){
-        for(int i=1;i<=clusterNum;i++){
-            List<Point> cluster=new ArrayList<>();
-            for(Point p:points){
-                if(p.getCid()==i){
+    private void getCenter(int clusterNum, List<Point> points) {
+        for (int i = 1; i <= clusterNum; i++) {
+            List<Point> cluster = new ArrayList<>();
+            for (Point p : points) {
+                if (p.getCid() == i) {
                     cluster.add(p);
                 }
             }
@@ -411,40 +440,41 @@ public class ClassifyService {
 
     /**
      * 计算中心点
+     *
      * @param cluster
      */
-    private void CalculateCenter(List<Point> cluster){
+    private void CalculateCenter(List<Point> cluster) {
 
-        double minDistance=10000;
-        int center=0;
-        int cluSize=cluster.size();
-        for(int i=0;i<cluSize;i++){
+        double minDistance = 10000;
+        int center = 0;
+        int cluSize = cluster.size();
+        for (int i = 0; i < cluSize; i++) {
             //平均距离
-            double weightDitstance=0;
-            float[] vec1=cluster.get(i).getVec();
-            for(int j=0;j<cluSize;j++){
-                double distance=0;
-                float[] vec2=cluster.get(j).getVec();
-                int size=vec1.length;
+            double weightDitstance = 0;
+            float[] vec1 = cluster.get(i).getVec();
+            for (int j = 0; j < cluSize; j++) {
+                double distance = 0;
+                float[] vec2 = cluster.get(j).getVec();
+                int size = vec1.length;
                 //计算i到j的距离
-                for(int k=0;k<size;k++){
+                for (int k = 0; k < size; k++) {
                     //System.out.println(floats[j]-floats1[j]);
-                    distance=distance+(vec1[j]-vec2[j])*(vec1[j]-vec2[j]);
+                    distance = distance + (vec1[j] - vec2[j]) * (vec1[j] - vec2[j]);
                 }
 
-                weightDitstance+=Math.sqrt(distance);
+                weightDitstance += Math.sqrt(distance);
 
             }
 
-            if(weightDitstance<minDistance){
-                center=i;
-                minDistance=weightDitstance;
+            if (weightDitstance < minDistance) {
+                center = i;
+                minDistance = weightDitstance;
             }
 
         }
 
-        for(int i=0;i<cluSize;i++){
-            if(i==center)
+        for (int i = 0; i < cluSize; i++) {
+            if (i == center)
                 cluster.get(i).setCenter(true);
             else
                 cluster.get(i).setCenter(false);
@@ -454,84 +484,87 @@ public class ClassifyService {
 
     /**
      * DBSCAN 算法
+     *
      * @param points
      * @param r
      * @param size
      * @return
      */
-    private int DBSCAN(List<Point> points,double r,int size) {
-        int clusterNum=0;
+    private int DBSCAN(List<Point> points, double r, int size) {
+        int clusterNum = 0;
 
         //
-        boolean allVisited=false;
+        boolean allVisited = false;
 
-        while(!allVisited){
+        while (!allVisited) {
 
-            for(Point point:points){
+            for (Point point : points) {
+                System.out.println("点" + point.getName());
                 //被访问就跳过
-                if(point.isVisited()){
+                if (point.isVisited()) {
                     continue;
                 }
-                allVisited=false;
+                allVisited = false;
                 point.setVisited(true);
-                List<Point> neighbors=getNeighbors(point,points,r);
+                List<Point> neighbors = getNeighbors(point, points, r);
                 //判断是中心点还是边界点
-                if(neighbors.size()<size){
+                if (neighbors.size() < size) {
                     //边界邻居太少 不形成簇 设置簇id为-1
-                    if(point.getCid()<=0) {
+                    if (point.getCid() <= 0) {
                         point.setCid(-1);
-                    }else{
-                        //形成新的簇
-                        //新的点
-                        if(point.getCid()<=0){
-                            clusterNum++;
-                            //合并成簇
-                            expandCluster(point,neighbors,clusterNum,points);
-                        }else {
-                            //被扩展过的点
-                            int cid=point.getCid();
-                            expandCluster(point,neighbors,cid,points);
-                        }
+                    }
+                } else {
+                    //形成新的簇
+                    //新的点
+                    if (point.getCid() <= 0) {
+                        clusterNum++;
+                        //合并成簇
+                        expandCluster(point, neighbors, clusterNum, points);
+                    } else {
+                        //被扩展过的点
+                        int cid = point.getCid();
+                        expandCluster(point, neighbors, cid, points);
                     }
                 }
-
-
             }
 
-            allVisited=true;
+            allVisited = true;
+
         }
 
+
         return clusterNum;
-    }
+}
 
     /**
      * 扩展合并簇
+     *
      * @param point
      * @param neighbors
      * @param cid
      * @param points
      */
 
-    public void expandCluster(Point point,List<Point> neighbors,int cid,List<Point> points){
+    public void expandCluster(Point point, List<Point> neighbors, int cid, List<Point> points) {
         //将该点簇id设置为cid
         point.setCid(cid);
 
-        for(Point p:neighbors){
+        for (Point p : neighbors) {
             //已经是某个簇成员了
-            if(!p.isVisited()){
+            if (!p.isVisited()) {
                 p.setVisited(true);
-                //获得p点的邻居
-                List<Point> pNeighbors=getNeighbors(p,points,0.4);
+                //获得p点的邻居 半径大小改为0.4
+                List<Point> pNeighbors = getNeighbors(p, points, 1);
 
                 //簇合并
-                if(pNeighbors.size()>2){
-                    for(Point np:pNeighbors){
+                if (pNeighbors.size() > 2) {
+                    for (Point np : pNeighbors) {
                         np.setCid(cid);
                     }
                 }
             }
             //q不是任何簇的成员
-            if(p.getCid()<=0){
+            if (p.getCid() <= 0) {
                 p.setCid(cid);
             }
 
@@ -540,26 +573,28 @@ public class ClassifyService {
 
     /**
      * 计算p点的所有邻居
+     *
      * @param p
      * @param points
      * @param r
      * @return
      */
     //算p点的邻居
-    public List<Point> getNeighbors(Point p,List<Point> points,double r){
-        List<Point> neighbors=new ArrayList<>();
-
-        for(Point point:points){
-            float[] vec1=p.getVec();
-            float[] vec2=point.getVec();
-            int size=vec1.length;
-            double distance=0;
-            for(int j=0;j<size;j++){
+    public List<Point> getNeighbors(Point p, List<Point> points, double r) {
+        List<Point> neighbors = new ArrayList<>();
+        System.out.println("半径大小"+r);
+        for (Point point : points) {
+            float[] vec1 = p.getVec();
+            float[] vec2 = point.getVec();
+            int size = vec1.length;
+            double distance = 0;
+            for (int j = 0; j < size; j++) {
                 //System.out.println(vec1[j]-vec2[j]);
-                distance=distance+(vec1[j]-vec2[j])*(vec1[j]-vec2[j]);
+                distance = distance + (vec1[j] - vec2[j]) * (vec1[j] - vec2[j]);
             }
+            System.out.println(p.getName()+" "+point.getName()+"距离之间"+Math.sqrt(distance));
             //如果距离小于半径
-            if(Math.sqrt(distance)<r)
+            if (Math.sqrt(distance) < r)
                 neighbors.add(point);
         }
 
@@ -569,24 +604,25 @@ public class ClassifyService {
 
     /**
      * 计算词1和词2的相似度
+     *
      * @param word1
      * @param word2
      * @return
      */
 
 
-    public double getSimilarity(String word1, String word2){
-        Word2VEC vec = new Word2VEC() ;
+    public double getSimilarity(String word1, String word2) {
+        Word2VEC vec = new Word2VEC();
 
         try {
-            vec.loadJavaModel("/Users/xixi/Desktop/SSM/library/javaVector") ;
+            vec.loadJavaModel("/Users/xixi/Desktop/SSM/library/javaVector");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 //		System.out.println(Arrays.toString(vec.getWordVector("漂亮")));
 
-        return vec.distanceWith2Words(word1,word2);
+        return vec.distanceWith2Words(word1, word2);
 
     }
 }
