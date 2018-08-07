e917a53ce9efe61c7e30d01c0a351683fa0154df
xixi <458927520@qq.com>
2018-08-07 13:27:16 +0800

    cxj 8.7 1.27

--- WebContent/js/Seckill.js
+++ WebContent/js/Seckill.js
@@ -206,7 +206,7 @@ function remindonclick(seckillProductId) {
 
 //设置提醒信息
 function setRemin(seckillProductId) {
-    var userId=1;
+    var userId=get_cookie("miUserId");
     $.ajax({
         url:"setRemind/"+userId+"/"+seckillProductId,
         type:"get",
@@ -215,4 +215,23 @@ function setRemin(seckillProductId) {
         {
         }
     });
+}
+
+
+//取得用户id
+function get_cookie(Name) {
+    var search = Name + "="//查询检索的值
+    var returnvalue = "";//返回值
+    if (document.cookie.length > 0) {
+        sd = document.cookie.indexOf(search);
+        if (sd!= -1) {
+            sd += search.length;
+            end = document.cookie.indexOf(";", sd);
+            if (end == -1)
+                end = document.cookie.length;
+            //unescape() 函数可对通过 escape() 编码的字符串进行解码。
+            returnvalue=unescape(document.cookie.substring(sd, end))
+        }
+    }
+    return returnvalue;
 }
